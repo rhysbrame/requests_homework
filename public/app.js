390e40297b578e2e71bc5d7d4ccddedc2c1c10dc
Rhys Brame <rhysbrame@gmail.com>
2017-05-17 16:31:04 +0100

    created make request and request complete functions. console.log gives the hash of albums

--- public/app.js
+++ public/app.js
@@ -1,5 +1,25 @@
 var app = function(){
 
+  makeRequest();
+}
+
+var requestComplete = function() {
+  if(this.status !== 200) return;
+
+  var jsonString = this.responseText;
+  var albums = JSON.parse(jsonString);
+  
+
+
+}
+
+var makeRequest = function() {
+  var url = "https://api.spotify.com/v1/search?q=christmas&type=album";
+  var request = new XMLHttpRequest();
+
+  request.open("GET", url);
+  request.addEventListener("load", requestComplete);
+  request.send();
 }
 
 window.addEventListener('load', app);
