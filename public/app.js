b3e98b7f3bdc1daf824fe93ababcf10384eeb7ef
Rhys Brame <rhysbrame@gmail.com>
2017-05-17 21:26:48 +0100

    added feetch search and got the div to clear every call

--- public/app.js
+++ public/app.js
@@ -1,25 +1,43 @@
 var app = function(){
-
-  makeRequest();
+  fetchSearch();
 }
 
-var requestComplete = function() {
-  if(this.status !== 200) return;
+var fetchSearch = function(){
+  var inputTag = document.getElementById("search-query");
 
-  var jsonString = this.responseText;
-  var albums = JSON.parse(jsonString);
-  
-
-
+  inputTag.addEventListener('input', function(event) {
+    makeRequest(event.target.value);
+  });
 }
 
-var makeRequest = function() {
+var makeRequest = function(serachedItem) {
+  console.log("item", serachedItem);
+  //i want to input the searchedItem into the url
   var url = "https://api.spotify.com/v1/search?q=christmas&type=album";
   var request = new XMLHttpRequest();
 
   request.open("GET", url);
   request.addEventListener("load", requestComplete);
   request.send();
+}
+
+var requestComplete = function() {
+  if(this.status !== 200) return;
+  var jsonString = this.responseText;
+  var albumHash = JSON.parse(jsonString);
+  var albums = albumHash.albums.items;
+  displayAlbums(albums);
+}
+
+var displayAlbums = function(albums) {
+  var divTag = document.getElementById('albums');
+  document.getElementById('albums').innerHTML = "";
+  for (album of albums) {
+    var li = document.createElement('li');
+    li.innerHTML = album.name;
+    divTag.appendChild(li);
+  } 
+
 }
 
 window.addEventListener('load', app);
